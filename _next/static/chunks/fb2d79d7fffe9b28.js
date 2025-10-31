a4661992564875e445e20e47b3ca6028b8804fcb
chenweigh <chenweigh@users.noreply.github.com>
2025-10-31 12:14:08 +0000

    deploy: ddd48659be0396cc99cea7661fdb82daffd878a7

---
+++ _next/static/chunks/fb2d79d7fffe9b28.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,49514,t=>{"use strict";var e=t.i(71645);function n(){return(0,e.useEffect)(()=>{let t=document.createElement("style");return t.textContent=`
+      @font-face {
+        font-family: 'DS-DIGIT';
+        src: url('/react-next-app/fonts/DS-DIGIT.TTF') format('truetype');
+        font-weight: normal;
+        font-style: normal;
+      }
+    `,document.head.appendChild(t),()=>{document.head.removeChild(t)}},[]),null}t.s(["default",()=>n])}]);
