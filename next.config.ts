6f62cf47ff005b3bf0ea6104b06ae6b527785aed
Eleven <chen158_7@126.com>
2025-11-07 15:51:22 +0800

    fix:test

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   //next start是一个nodejs服务器，静态导出后，你就不再需要nodejs服务器，只需要一个静态文件服务器
   // output: 'export', // 关键：启用静态导出
   output: basePath ? 'export' : undefined,
-  trailingSlash: true, // 确保 URL 以斜杠结尾
+  trailingSlash: false, // 确保 URL 以斜杠结尾
   reactStrictMode: false,
   generateEtags: false,
   poweredByHeader: false,
