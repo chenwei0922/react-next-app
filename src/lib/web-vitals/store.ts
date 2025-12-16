8c83e0dd0230e7beec8c6b3e00bbb68f612a4db9
Eleven <chen158_7@126.com>
2025-12-16 14:30:02 +0800

    feat: test vital

--- src/lib/web-vitals/store.ts
+++ src/lib/web-vitals/store.ts
@@ -2,12 +2,14 @@ import { VitalStore } from './type';
 import { memoryStore } from './memory';
 import { analyticsStore } from './analytics';
 
-export function getVitalStore(env?: { ANALYTICS?: unknown }): VitalStore {
+export function getVitalStore(env?: { web_vitals?: unknown }): VitalStore {
   // Cloudflare Edge
-  if (env?.ANALYTICS) {
+  if (env?.web_vitals) {
+    console.log('Using Cloudflare Edge analytics');
     return analyticsStore(env);
   }
 
   // 本地 / Node
+  console.log('Using memory store');
   return memoryStore;
 }
