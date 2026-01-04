7bf750a1e83cb0eaca53297f7adbc7a5bc336a59
Eleven <chen158_7@126.com>
2026-01-04 18:11:57 +0800

    feat: 升级next & tailwindcss写法

--- sh/upgrade-next.sh
+++ sh/upgrade-next.sh
@@ -5,13 +5,13 @@ echo "开始升级 Next.js..."
 cp package.json package.json.bak
 
 # 升级核心包
-npm install next@latest react@latest react-dom@latest
+pnpm install next@latest react@latest react-dom@latest
 
 # 升级开发依赖
-npm install --save-dev @types/react@latest @types/react-dom@latest eslint-config-next@latest
+pnpm install --save-dev @types/react@latest @types/react-dom@latest eslint-config-next@latest
 
 # 清理并重新安装
 rm -rf node_modules package-lock.json
-npm install
+pnpm install
 
 echo "升级完成！"
